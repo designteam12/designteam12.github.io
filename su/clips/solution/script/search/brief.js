e63f1bdab84d7e8f6bea382a9a2ea0cccc4a566f
designteam12 <118249471+designteam12@users.noreply.github.com>
2023-01-30 14:22:45 +0900

    Add files via upload

--- su/clips/solution/script/search/brief.js
+++ su/clips/solution/script/search/brief.js
@@ -1,7 +1,7 @@
 $(function(){
 	//사이즈 변수 선언(공통 사용)
 	var winWidth = window.innerWidth || document.documentElement.clientWidth;//미디어쿼리 사이즈와 $(window).width()가 인식하는 px단위 사이즈가 달라서 선언한 변수 : clinentWidth 와 innerWidth의 사이즈는 동일하나 innerWidth는 익스플로러 9 미만 버전은 인식하지 못하므로 동시선언
-
+	
 	//상세검색 레이어
 	$('.detailSearchBtn').on('click',function(){
 		$('.detailSearchLayer').addClass('on');
@@ -194,7 +194,6 @@ $(function(){
 			$('.holdingInfo .mobileTable .footable-row-detail').css('display','none')
 		}
 	});
-	//footable-detail-show
 	// 서가부재도서 신청 레이어 팝업
 
 
